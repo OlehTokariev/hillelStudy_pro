061791ceb35c1d22ad6568503acd8820ec3b0e32
OlehTokariev <tokarevolega95@gmail.com>
2024-12-23 00:06:51 +0200

    Small improvements

--- lesson_001/src/main/java/org/example/Main.java
+++ lesson_001/src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
 public class Main {
+    private static final double CONV_K = 2.20462;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Converter App.");
+
+        double kgs = 5;
+        double pounds = convKgsToPounds(kgs);
+        System.out.println("Result is " + pounds + " pounds.");
+    }
+
+    private static double convKgsToPounds(double kgs) {
+        return kgs * CONV_K;
     }
 }
