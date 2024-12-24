2ea881d9d9521f5d13758f19e0b9ce6536b0ef82
OlehTokariev <tokarevolega95@gmail.com>
2024-12-24 15:15:40 +0200

    Set up project and basic info output

--- hillel_2025/src/main/java/org/example/lesson2/Main.java
+++ hillel_2025/src/main/java/org/example/lesson2/Main.java
@@ -1,10 +1,11 @@
-package org.example;
+package org.example.lesson2;
 
 public class Main {
     private static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {
-        System.out.println("Converter App.");
+        System.out.println("Welcome to the Converter App!");
+        System.out.println("This application converts units between miles and kilometers.");
 
         double kgs = 5;
         double pounds = convKgsToPounds(kgs);
